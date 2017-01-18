2d05eb8aac6ad4c4bd2e96fbfc19fc87a79dd226
Anush Shetty <ashetty@redhat.com>
2017-01-18 17:01:29 +0530

    Adding kompose up/down tests

---
+++ tests/redis/run.sh
@@ -0,0 +1,38 @@
+# Test case for kompose up/down
+
+LOG_FILE=$1
+source ./lib.sh
+
+create_log "[KOMPOSE] Running redis compose file: tests/redis/docker-compose.yml"
+kompose --provider=openshift -f tests/redis/docker-compose.yml up &>> $LOG_FILE; result=$?;
+
+if [ $result -ne 0 ]; then
+    create_log "Kompose command failed"
+    exit 1;
+fi
+
+create_log "Waiting for the pods to come up"
+
+# TODO: fix this
+sleep 200;
+
+if [ $(oc get pods | grep redis | awk '{ print $3 }') == 'Running'  ] &&
+       [ $(oc get pods | grep web | awk '{ print $3 }') == 'Running'  ] ; then
+    create_log "[KOMPOSE] All Almighty pods are Running"
+    oc get pods >> $LOG_FILE
+fi
+
+# Kompose down
+kompose --provider=openshift -f tests/redis/docker-compose.yml down &>> $LOG_FILE; result=$?;
+
+if [ $result -ne 0 ]; then
+    create_log "Kompose down command failed"
+    exit 1;
+fi
+
+sleep 200;
+
+if [ $(oc get pods | wc -l ) -eq 0 ] ; then
+    create_log "[KOMPOSE] All pods are down"
+    exit 1;
+fi
